78d385c5713011e7bae592f9afa03e0f72b74de0
XIU <csc940218@gmail.com>
2022-11-06 23:20:41 +0800

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: proces.env.PGUSER,
+    pgHost: proces.env.PGHOST,
+    pgDatabase: proces.env.PGDATABASE,
+    pgPassword: proces.env.PGPASSWORD,
+    pgPort: proces.env.PGPORT,
+
+
+};
